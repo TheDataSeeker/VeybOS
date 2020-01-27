c4ceb1d7793021c0bfeb269ed5180ca76af2ea34
TheDataSeeker <51262107+TheDataSeeker@users.noreply.github.com>
2020-01-28 06:16:36 +1000

    Added basic concept

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // Not sure if this is required but I tend to include it anyway.
 const fs = require("fs");
+const MKD = require("mkdcolor");
+
+function Show(name) {
+  switch(name) {
+
+    default: 
+      putLang("error_noNamedFound");
+      break;
+  }
+}
