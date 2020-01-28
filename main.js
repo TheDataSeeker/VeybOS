3b7ca846ccb5b27e6c6a39e2aec5b5497f609cd6
TheDataSeeker <51262107+TheDataSeeker@users.noreply.github.com>
2020-01-29 03:51:46 +1000

    Main update. Still gotta fix tho.

--- main.js
+++ main.js
@@ -15,29 +15,29 @@ App.Put = (string) => {
 // define globally
 const Put = App.Put;
 
-App.AwaitInput = function(question = "", cb = () => {App.ShowLang("error_NoAWICallback")}) {
+App.AwaitInput = function(question = "", callback = (v) => {App.ShowLang("error_NoAWICallback")}) {
   // TODO: Test if this is better within self function or externally.
   var Interface = Readline.createInterface({input: process.stdin,output: process.stdout});
   Interface.question(question, (response) => {
-    cb(response);
+    callback(response);
     Interface.close();
   });
 }
 
 // prints a MKD color coded file to the console. /data/lang/
 // naming format is <class>_<nickname> ; for example: menu_main, menu_help, error_timedout, error_fileread
-App.ShowLang = function(name) {
+App.ShowLang = function(name = "", key1 = "", key2 = "", key3 = "", ) {
   // Wrapping in try catch for now to prevent unexpected problems.
   // TODO: Fool'proof this.
   try {
     let TargetFile = `./data/lang/${name}.lang`;
     // validate if that lang file exists else summon error.
-    if(!fs.exists(TargetFile)) {
+    if(!fs.existsSync(TargetFile)) {
       Put(`[#-r]INTERNAL ERROR -- App.ShowLang CANNOT FIND ${name} (${TargetFile})`);
       return;
     }
     // TODO: check if async or sync is best
-    Put(fs.readFile(TargetFile));
+    Put(fs.readFileSync(TargetFile,{encoding:"UTF-8"}).replace(/\$1/igm,key1).replace(/\$2/igm,key2).replace(/\$3/igm,key3));
     return;
   } catch (err) {
     // Just using console.log for now since im lazy.
@@ -46,17 +46,24 @@ App.ShowLang = function(name) {
 }
 
 // prints pre programmed menu to console.
-App.ShowMenu = function(name) {
-  switch(name) {
+App.ShowMenu = function(menu) {
+  switch(menu) {
     case "Main":
       App.ShowLang("menu_main");
-      
+      App.AwaitInput("",(res)=>{
+        if(res == "about") {
+          App.ShowLang("text_about");
+        } else {
+          App.ShowLang("error_InvalidInput",res);
+        }
+        App.ShowMenu(menu);
+      });
       break;
     default: 
-      App.ShowLang("error_NoNamedMenu");
+      App.ShowLang("error_NoNamedMenu",menu);
       break;
   }
 }
 
 // Start it all
-App.ShowMenu("main");
+App.ShowMenu("Main");
